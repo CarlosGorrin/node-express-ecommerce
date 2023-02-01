043bd008725e45efa54770592ba2c96949ed9900
CarlosGorrin <carlos.gorrin@gmail.com>
2023-02-01 15:21:43 -0300

    servicios y manejo de errores

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routerApi = require('./routes');
 
+const { logErrors, errorHandler } = require('./middlewares/error.handler')
+
 const app = express();
 const port = 3000;
 
@@ -11,11 +13,14 @@ app.get('/', (req, res) => {
 });
 
 app.get('/page2', (req, res) => {
-  res.send('Soy la pagina DOS');
+  res.send('Soy la pagina editada de prueba');
 });
+
+routerApi(app);
+
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log('Port' + port);
 });
-
-routerApi(app);
--- routes/products.router.js
+++ routes/products.router.js
@@ -1,65 +1,49 @@
 const express = require('express');
-const faker = require('faker');
+
+const ProductsService = require('./../services/product.service')
 
 const router = express.Router();
+const service = new ProductsService();
 
-router.get('/', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    });
-  }
-  res.json(products)
+router.get('/', async (req, res) => {
+  const products = await service.find();
+  res.json(products);
 });
 
 router.get('/filter', (req, res) => {
   res.send('Filter')
-})
-
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
-  if (id === '999') {
-    res.status(404).json({
-      message: 'not found'
-    });
-  } else {
-    res.status(200).json({
-      id,
-      name: 'Soap',
-      price: 4.35
-    })
-  }
 });
 
-router.post('/', (req, res) => {
-  const body = req.body;
-  res.status(201).json({
-    message: 'created',
-    data: body
-  });
-})
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const product = await service.findOne(id);
+  res.json(product);
+});
 
-router.patch('/:id', (req, res) => {
-  const { id } = req.params
+router.post('/', async (req, res) => {
   const body = req.body;
-  res.json({
-    message: 'update',
-    data: body,
-    id,
-  });
-})
+  const newProduct = await service.create(body)
+  res.status(201).json(newProduct);
+});
 
-router.delete('/:id', (req, res) => {
+router.patch('/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const body = req.body;
+    const product = await service.update(id, body);
+    res.json(product);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message
+    });
+  }
+
+});
+
+router.delete('/:id', async (req, res) => {
   const { id } = req.params
-  res.json({
-    message: 'delete',
-    id,
-  });
-})
+  const respuesta = await service.delete(id);
+  res.json(respuesta);
+});
 
 module.exports = router;
